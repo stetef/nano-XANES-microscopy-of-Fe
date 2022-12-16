ecc4ca879769f512b879bd478452664edae6f289
Samantha Tetef <stetef@u.rochester.edu>
2022-12-16 10:46:36 -0800

    backup

--- utils.py
+++ utils.py
@@ -44,6 +44,36 @@ from joblib import dump, load
 
 import umap
 
+# from https://en.wikipedia.org/wiki/Help:Distinguishable_colors
+discrete_cmap = [
+    (240,163,255), 
+    (0,117,220),
+    (153,63,0),
+    (76,0,92),
+    (25,25,25),
+    (0,92,49),
+    (43,206,72),
+    (255,204,153),
+    (128,128,128),
+    (148,255,181),
+    (143,124,0),
+    (157,204,0),
+    (194,0,136),
+    (0,51,128),
+    (255,164,5),
+    (255,168,187),
+    (66,102,0),
+    (255,0,16),
+    (94,241,242),
+    (0,153,143),
+    (224,255,102),
+    (116,10,255),
+    (153,0,0),
+    (255,255,128),
+    (255,225,0),
+    (255,80,5)
+]
+discrete_cmap = [tuple([c / 255. for c in color]) for color in discrete_cmap]
 
 def parse_tiff(filename):
     img = Image.open(filename)
